9597c62935452a778b4a71cea7909ba931be8102
Sara Coni <conisara96@gmail.com>
2025-02-26 10:33:40 -0300

    Aula sobre concatenaçao em php

---
+++ aula02/concatenar.php
@@ -0,0 +1,16 @@
+<?php
+
+ECHO "Hello Word! <br>";  
+echo "Hello Word! <br>"; 
+EcHo "Hello Word! <br>"; 
+
+$nome = "João";
+$sobrenome = "Siles";
+$numero = 4;
+
+echo $nome . "lindo" . "<br>";
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . "<p>" . $nome . 
+" " . $sobrenome . "</p>";
+
+?>
