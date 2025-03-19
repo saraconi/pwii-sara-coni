d632498470c9d229a754f2d33e2d513aeb229f03
Sara Coni <conisara96@gmail.com>
2025-03-19 10:30:18 -0300

    While, do e for

---
+++ aula04/repeticao.php
@@ -0,0 +1,25 @@
+<?php
+
+$i = 1;
+
+while ($i < 6){
+    echo $i;
+    echo "<br>";
+    $i++;
+}
+
+echo "--------- <br>";
+
+$i = 7;
+do {
+    echo $i;
+    $i++;
+  } while ($i < 6);
+
+  for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+  }
+
+
+
+?>
