f895a36f1e866c6205c7d70250b92a0873c8df49
Sara Coni <conisara96@gmail.com>
2025-04-09 10:33:16 -0300

    Aula 05, função

---
+++ aula05/f.js
@@ -0,0 +1,7 @@
+
+function soma (n1, n2) {
+    console.log(n1+n2)
+}
+soma (2, 3);
+
+
---
+++ aula05/funcao.php
@@ -0,0 +1,22 @@
+<?php
+
+function myMessage() {
+    echo "Hello world! <br>";
+  }
+  
+  myMessage();
+  myMessage(); 
+
+function soma ($n1, $n2) {
+   echo $n1 + $n2, "<br>";
+}
+
+  soma(3, 4);
+  soma (3, 10);
+  soma (4, 40);
+
+  function cadastrar (){
+      // vai ser uma logica par cadatrar 
+  }
+  
+?>
